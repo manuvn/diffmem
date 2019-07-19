0946ef86f5c2656f7b6ebf9f7533554626fd6c93
mnair <mnair@ini.uzh.ch>
2019-07-19 16:19:15 +0200

    Add ternary

--- mnist.py
+++ mnist.py
@@ -32,6 +32,40 @@ def ClipW(weight):
     weight = where(weight <= -1, -1, weight)
     return weight
 
+def Rectify(weight):
+    weight = where(weight <= 0.01, 0.01, weight)
+    return weight
+
+tern_boundary=0.05
+class Ternarize(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, weight):
+        # probs = torch.tanh(weight)
+        # binarize the weight
+        weight_t = where(weight >= 0, 1, -1)
+        weight_t = where((weight >= -tern_boundary)&(weight <= tern_boundary), 0, weight_t)
+        ctx.save_for_backward(weight)
+        return weight_t
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # This is a pattern that is very convenient - at the top of backward
+        # unpack saved_tensors and initialize all gradients w.r.t. inputs to
+        # None. Thanks to the fact that additional trailing Nones are
+        # ignored, the return statement is simple even when the function has
+        # optional inputs.
+        weight = ctx.saved_tensors
+        grad_weight = None
+
+        # These needs_input_grad checks are optional and there only to
+        # improve efficiency. If you want to make your code simpler, you can
+        # skip them. Returning gradients for inputs that don't require it is
+        # not an error.
+        if ctx.needs_input_grad[0]:
+            grad_weight = grad_output
+        return grad_weight
+ternarize = Ternarize.apply
+
 class Binarize(torch.autograd.Function):
     @staticmethod
     def forward(ctx, weight):
@@ -66,7 +100,7 @@ class BinMem(Binarize):
     def forward(ctx, weight):
         # probs = torch.tanh(weight)
         # binarize the weight
-        weight_b = where(weight >= 0, 1, 0.0)
+        weight_b = where(weight >= 0, 1, 0.01)
         ctx.save_for_backward(weight)
         return weight_b
 binmem = BinMem.apply
@@ -95,6 +129,30 @@ class NoisyBinLinear(nn.Module):
         randweight = rand + weight_b
         return input.mm(randweight)
 
+class TernLinear(nn.Module):
+    def __init__(self, num_ip, num_op):
+        super(TernLinear, self).__init__()
+        self.weight = nn.Parameter(torch.empty(num_ip, num_op).uniform_(), requires_grad=True)
+
+    def forward(self, input):
+        with torch.no_grad():
+            self.weight.data = ClipW(self.weight.data)
+        weight_b = ternarize(self.weight)
+        return input.mm(weight_b)
+
+class NoisyTernLinear(nn.Module):
+    def __init__(self, num_ip, num_op, sigma=0.2):
+        super(NoisyTernLinear, self).__init__()
+        self.weight = nn.Parameter(torch.empty(num_ip, num_op).uniform_(), requires_grad=True)
+        self.sigma = sigma
+    def forward(self, input):
+        with torch.no_grad():
+            self.weight.data = ClipW(self.weight.data)
+        weight_t  = ternarize(self.weight)
+        rand = torch.randn_like(weight_t) * self.sigma * tern_boundary
+        randweight = rand + weight_t
+        return input.mm(randweight)
+
 class DiffMemLinear(nn.Module):
     def __init__(self, num_ip, num_op, sigma=0.05):
         super(DiffMemLinear, self).__init__()
@@ -103,8 +161,8 @@ class DiffMemLinear(nn.Module):
         self.sigma = sigma
     def forward(self, input):
         with torch.no_grad():
-            self.weighta.data = ClipW(self.weighta.data)
-            self.weightb.data = ClipW(self.weightb.data)
+            self.weighta.data = Rectify(self.weighta.data)
+            self.weightb.data = Rectify(self.weightb.data)
         b_weighta  = binmem(self.weighta)
         b_weightb  = binmem(self.weightb)
         randa = torch.randn_like(b_weighta) * self.sigma
@@ -112,6 +170,22 @@ class DiffMemLinear(nn.Module):
         randweight = randa + b_weighta - randb - b_weightb
         return input.mm(randweight)
 
+class DiffMemNormLinear(nn.Module):
+    def __init__(self, num_ip, num_op, sigma=0.05):
+        super(DiffMemNormLinear, self).__init__()
+        self.weighta = nn.Parameter(torch.empty(num_ip, num_op).uniform_(), requires_grad=True)
+        self.weightb = nn.Parameter(torch.empty(num_ip, num_op).uniform_(), requires_grad=True)
+        self.sigma = sigma
+    def forward(self, input):
+        with torch.no_grad():
+            self.weighta.data = Rectify(self.weighta.data)
+            self.weightb.data = Rectify(self.weightb.data)
+        b_weighta  = binmem(self.weighta)
+        b_weightb  = binmem(self.weightb)
+        randa = torch.randn_like(b_weighta) * self.sigma
+        randb = torch.randn_like(b_weightb) * self.sigma
+        randweight = randa + b_weighta - randb - b_weightb/(randa + b_weighta + randb + b_weightb)
+        return input.mm(randweight)
 
 class Net(nn.Module):
     def __init__(self, nntype='Linear', nunits=50, nhidden=3, sigma=0.2):
@@ -123,8 +197,17 @@ class Net(nn.Module):
             fc = BinLinear
         elif nntype == 'NoisyBinary':
             fc = NoisyBinLinear
+            kwargs['sigma'] = sigma
+        elif nntype == 'Ternary':
+            fc = TernLinear
+        elif nntype == 'NoisyTernary':
+            fc = NoisyBinLinear
+            kwargs['sigma'] = sigma
         elif nntype == 'DiffMem':
             fc = DiffMemLinear
+            kwargs['sigma'] = sigma
+        elif nntype == 'DiffMemN':
+            fc = DiffMemNormLinear
             kwargs['sigma'] = sigma
         else:
             fc = nn.Linear
@@ -218,14 +301,14 @@ if __name__ == '__main__':
 
     # tensorboard --logdir logs
     parser = argparse.ArgumentParser(description='MNIST binary MLP')
-    parser.add_argument('--nntype', default='Binary', type=str, help='Linear, Binary, NoisyBinary, DiffMem')
+    parser.add_argument('--nntype', default='Binary', type=str, help='Linear, Binary, NoisyBinary, Ternary, NoisyTernary, DiffMem, DiffMemN')
     parser.add_argument('--nunits', default=1024, type=int, help='Number of units in hidden layer')
     parser.add_argument('--nhidden', default=3, type=int, help='Number of hidden layers')
     parser.add_argument('--batch_size', default=200, type=int, help='Batch size')
     parser.add_argument('--nepochs', default=300, type=int, help='Number of training epochs')
     parser.add_argument('--lr', default=0.01, type=float, help='Initial learning rate')
     parser.add_argument('--sigma', default=0.1, type=float, help='Diff mem spread')
-    parser.add_argument('--lr_patience', default=50, type=int, help='Learning rate patience')
+    parser.add_argument('--lr_patience', default=20, type=int, help='Learning rate patience')
     parser.add_argument('--logpath', type=str, default='./logs/', 
                             help='Save path for the logs')
     parser.add_argument('--cuda', type=str, default='y',
@@ -233,7 +316,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     T = time.strftime("M%mD%dH%HM%M")
-    tboard_dir = args.logpath+args.nntype+'_B'+str(args.batch_size)+'_H'+str(args.nhidden)+'_N'+str(args.nhidden)+'_lr'+str(args.lr)+'-Time-'+T
+    tboard_dir = args.logpath+args.nntype+'_B'+str(args.batch_size)+'_H'+str(args.nhidden)+'_N'+str(args.nunits)+'_S'+str(args.sigma)+'_lr'+str(args.lr)+'-Time-'+T
     # writer = SummaryWriter('./logs')
     writer = SummaryWriter(tboard_dir)
 
