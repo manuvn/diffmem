94a1658fe1c10d541028bee09310def716a4d5ff
mnair <mnair@ini.uzh.ch>
2019-08-06 22:44:05 +0200

    Update nunits in script

--- script.bat
+++ script.bat
@@ -1,8 +1,8 @@
-python mnist.py --nntype=Linear --nunits=128 --nhidden=3 --batch_size=100
-python mnist.py --nntype=NoisyBinary --nunits=128 --nhidden=3 --batch_size=100 --sigma=0.1
-python mnist.py --nntype=NoisyBinary --nunits=128 --nhidden=3 --batch_size=100 --sigma=0.3
-python mnist.py --nntype=NoisyBinary --nunits=128 --nhidden=3 --batch_size=100 --sigma=0.5
-python mnist.py --nntype=NoisyBinary --nunits=128 --nhidden=3 --batch_size=100 --sigma=1
-python mnist.py --nntype=NoisyBinary --nunits=128 --nhidden=3 --batch_size=100 --sigma=1.5
-python mnist.py --nntype=NoisyBinary --nunits=128 --nhidden=3 --batch_size=100 --sigma=2
+python mnist.py --nntype=Linear --nunits=512 --nhidden=3 --batch_size=100
+python mnist.py --nntype=NoisyBinary --nunits=512 --nhidden=3 --batch_size=100 --sigma=0.1
+python mnist.py --nntype=NoisyBinary --nunits=512 --nhidden=3 --batch_size=100 --sigma=0.3
+python mnist.py --nntype=NoisyBinary --nunits=512 --nhidden=3 --batch_size=100 --sigma=0.5
+python mnist.py --nntype=NoisyBinary --nunits=512 --nhidden=3 --batch_size=100 --sigma=1
+python mnist.py --nntype=NoisyBinary --nunits=512 --nhidden=3 --batch_size=100 --sigma=1.5
+python mnist.py --nntype=NoisyBinary --nunits=512 --nhidden=3 --batch_size=100 --sigma=2
 python plot_mnist.py
